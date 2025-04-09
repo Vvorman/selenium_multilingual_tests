0096a46accd8a1f41f8e969eef428c468a44a76d
Daria <69599637+Vvorman@users.noreply.github.com>
2025-04-10 01:52:23 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,10 +1,12 @@
-from selenium.webdriver.common.by import By
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 import time
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_guest_should_see_add_to_basket_button(browser):
     browser.get(link)
-    # time.sleep(30) # Раскомментируйте эту строку для визуальной проверки французской версии
-    button = browser.find_element(By.CSS_SELECTOR, ".add-to-basket button")
+    time.sleep(5)
+    button = browser.find_element('xpath', "//button[@class='btn btn-lg btn-primary btn-add-to-basket']")
     assert button.is_displayed(), "Add to basket button is not displayed"
